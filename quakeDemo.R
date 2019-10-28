5c47f24852ecdd16a52c1aa1b12faa8772b71dce
Joshua Wolff <joshuawolff@Joshuas-MBP-2.fios-router.home>
2019-10-27 20:35:13 -0400

    retest

--- quakeDemo.R
+++ quakeDemo.R
@@ -1,4 +1,4 @@
 quakedf <- read.csv("quakes.csv")
 
 quakeStats <- as.data.frame(summary(quakedf))
-write.csv(quakeStats,"quake_stats.csv")
+write.csv(quakeStats,"quake_stats.csv",row.names = FALSE)
